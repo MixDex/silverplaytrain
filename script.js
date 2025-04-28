1225e74d56e67dedf51902cf878951dfac2cc1ae
MixDex <163492754+MixDex@users.noreply.github.com>
2025-04-28 16:40:53 +0300

    Add files via upload
    For my site

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const sections = document.querySelectorAll('.page');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.25
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
